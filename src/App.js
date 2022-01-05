8c5c2e5e3156e4ba3e2a470a1059f84ad1d97c0c
gonzoespinho10 <fidalgogonzo@gmail.com>
2022-01-05 14:43:22 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
+import React, {useEffect, useState} from "react";
+import AccordionBodyItem from "./components/Accordion/AccordionBodyItem";
 
 function App() {
 
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <div className="App">
-    <Navbar></Navbar>
-    </div>
-  );
+    const x_rapidapi_host = "api-football-v1.p.rapidapi.com";
+    const x_rapidapi_key = "24ef27f2b9msh1b6bcf01dfa7732p1c6ca1jsnfdf40d57abad";
+
+
+  useEffect(() => {
+      const fetchData = async () => {
+          setIsLoading(true)
+          try {
+              const res = await fetch("https://api-football-v1.p.rapidapi.com/v3/standings?season=2021&league=94", {
+                  "method": "GET",
+                  "headers": {
+                      "x-rapidapi-host": x_rapidapi_host,
+                      "x-rapidapi-key": x_rapidapi_key
+                  }
+              });
+              const json = await res.json()
+              const teams = json.response[0].league.standings[0];
+              setData(teams)
+              setIsLoading(false);
+          }
+              catch(error) {
+                  console.log("Error")
+              }
+              setIsLoading(false);
+      }
+   fetchData()}, [])
+
+
+
+    return (
+        isLoading ? (<p>Loading ...</p>)
+            :
+            (
+        <div className="App">
+            <Navbar></Navbar>
+            <AccordionBodyItem teams = {data}></AccordionBodyItem>
+        </div>)
+    );
+
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,26 +3,23 @@ import PropTypes from 'prop-types';
 
 const Navbar = () => {
     return (
-        <div>
+
             <nav className="navbar navbar-expand-lg">
                 <div className="row">
                     <div id="rules" className="col-md-4">
                         <button id="rulesBtn" className="btn btn-lg btn-outline-light">Rules</button>
                     </div>
                     <div className="col-md-4">
-                        <h1 id="titulo" className="display-5">Faz a tua equipa</h1>
+                        <h1 id="titulo" className="display-5 text-white">Faz a tua equipa</h1>
                     </div>
-
                     <div className="col-md-4 check-team">
-                        <span style={{display: "flex", alignItems: "center", color: "white"}}>MY TEAM</span>
-                        <a href="" type="button" className="btn img-thumbnail float-right">
-                            <img alt="soccer" id="myTeam" style={{height: "70px"}}/>
-                        </a>
+                        <div id="rules" className="col-md-4">
+                            <button id="rulesBtn" className="btn btn-lg btn-outline-light">My Team</button>
+                        </div>
                     </div>
                 </div>
-
             </nav>
-        </div>
+
     );
 };
 
